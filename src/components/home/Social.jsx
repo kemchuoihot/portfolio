aa24b5c1e9307a561fe78ad7550929eff87c9b09
kemchuoihot <thinhpx33@gmail.com>
2024-07-14 09:39:24 +0700

    create react js and making header

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.instagram.com/thinh.npt/" className="home__social-icon" targer="_blank"><i class="uil uil-instagram"></i></a>
+            <a href="https://www.linkedin.com/in/thinh-nguyen-21ba67284/" className="home__social-icon" targer="_blank"><i class="uil uil-linkedin-alt"></i></a>
+            <a href="https://github.com/kemchuoihot" className="home__social-icon" targer="_blank"><i class="uil uil-github-alt"></i></a>
+        </div>
+    );
+};
+
+export default Social;
