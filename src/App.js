38b903bd1bd3dd9554ea90c4c8b380636a6ed4aa
kemchuoihot <thinhpx33@gmail.com>
2024-07-15 21:59:54 +0700

    add about and skills section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home.jsx';
+import About from './components/about/About.jsx';
+import Skills from './components/skills/Skills.jsx';
+import Services from './components/services/Services.jsx';
 function App() {
   return (
     <>
     <Header />
     <main className="main">
-    <Home />
+      <Home />
+      <About />
+      <Skills />
+      <Services />
     </main>
     </>
   );
---
+++ src/components/about/Infor.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Infor = () => {
+    return (
+        <div className="about__infor grid">
+            <div className="about__box">
+                <i className="bx bx-award about-icon"></i>
+                <h3 className="about__title">Experience</h3>
+                <span className="about__subtitle">1 ...</span>
+            </div>
+            <div className="about__box">
+                <i className="bx bx-briefcase-alt about-icon"></i>
+                <h3 className="about__title">Project</h3>
+                <span className="about__subtitle">...</span>
+            </div>
+            <div className="about__box">
+                <i className="bx bx-support about-icon"></i>
+                <h3 className="about__title">Support</h3>
+                <span className="about__subtitle">24/7</span>
+            </div>
+        </div>
+    );
+};
+
+export default Infor;
