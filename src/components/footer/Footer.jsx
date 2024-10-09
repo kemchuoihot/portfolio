ad61719c306f9cfcea5ff762e8f3ebbf50be589d
kemchuoihot <thinhpx33@gmail.com>
2024-10-09 21:49:22 +0700

    change duration for 1000

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,36 +1,42 @@
-import React, { useEffect } from 'react';
-import './footer.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React, { useEffect } from "react";
+import "./footer.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Footer = () => {
-    useEffect(() => {
-        AOS.init({
-            duration: 3000,
-        });
-        // Refresh AOS on component mount
-        AOS.refresh();
-    }, []);
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+    // Refresh AOS on component mount
+    AOS.refresh();
+  }, []);
 
-    return (
-        <footer className="footer" data-aos="zoom-in">
-            <div className="footer__container container">
-                <h1 className="footer__title">Kindy</h1>
+  return (
+    <footer className="footer" data-aos="zoom-in">
+      <div className="footer__container container">
+        <h1 className="footer__title">Kindy</h1>
 
-                <ul className="footer__list">
-                    <li>
-                        <a href="#about" className="footer__link">About</a>
-                    </li>
-                    <li>
-                        <a href="#project" className="footer__link">Projects</a>
-                    </li>
-                    <li>
-                        <a href="#testimonals" className="footer__link">Testimonals</a>
-                    </li>
-                </ul>
-            </div>
-        </footer>
-    );
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#project" className="footer__link">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#testimonals" className="footer__link">
+              Testimonals
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,52 +1,66 @@
-import React,{useEffect} from 'react';
-import './project.css';
-import Film from '../../assets/film.png';
-import POS from '../../assets/POS.png';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React, { useEffect } from "react";
+import "./project.css";
+import Film from "../../assets/film.png";
+import POS from "../../assets/POS.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Project = () => {
-    useEffect(() => {
-        AOS.init({
-            duration: 3000,
-        });
-        // Refresh AOS on component mount
-        AOS.refresh();
-    }, []);
-    return (
-        <section className="project section" id="project" data-aos="zoom-in-up">
-            <h2 className="section__title">Project</h2>
-            <span className="section__subtitle">My recent projects</span>
-            <div className="project__container container grid">
-                <div className="project__card">
-                    <img className='project__img' src={POS} alt="" srcset="" />
-                    <h3 className="project__title">POS</h3>
-                    <div className="project__link">
-                        <a href="https://deploy-pos.vercel.app/login" className="project__button">
-                        <i class='bx bx-link'></i>  Demo <i className="project__button-icon bx bx-right-arrow-alt"></i>
-                        </a>
-                        <a href="https://github.com/kemchuoihot/POS" className="project__button">
-                        <i class='bx bxl-github' ></i>Github <i className="project__button-icon bx bx-right-arrow-alt"></i>
-                        </a>
-                    </div>
-                    
-                </div>
-                <div className="project__card">
-                    <img className='project__img' src={Film} alt="" srcset="" />
-                    <h3 className="project__title">PHP web films</h3>
-                    <div className="project__link">
-                        <a href="https://movie-city-2024.000webhostapp.com/" className="project__button">
-                            <i class='bx bx-link'></i> Demo <i className="project__button-icon bx bx-right-arrow-alt"></i>
-                        </a>
-                        <a href="https://github.com/kemchuoihot/city-movie" className="project__button">
-                            <i class='bx bxl-github' ></i>Github <i className="project__button-icon bx bx-right-arrow-alt"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-        </section>
-    );
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+    // Refresh AOS on component mount
+    AOS.refresh();
+  }, []);
+  return (
+    <section className="project section" id="project" data-aos="zoom-in-up">
+      <h2 className="section__title">Project</h2>
+      <span className="section__subtitle">My recent projects</span>
+      <div className="project__container container grid">
+        <div className="project__card">
+          <img className="project__img" src={POS} alt="" srcset="" />
+          <h3 className="project__title">POS</h3>
+          <div className="project__link">
+            <a
+              href="https://deploy-pos.vercel.app/login"
+              className="project__button"
+            >
+              <i class="bx bx-link"></i> Demo{" "}
+              <i className="project__button-icon bx bx-right-arrow-alt"></i>
+            </a>
+            <a
+              href="https://github.com/kemchuoihot/POS"
+              className="project__button"
+            >
+              <i class="bx bxl-github"></i>Github{" "}
+              <i className="project__button-icon bx bx-right-arrow-alt"></i>
+            </a>
+          </div>
+        </div>
+        <div className="project__card">
+          <img className="project__img" src={Film} alt="" srcset="" />
+          <h3 className="project__title">PHP web films</h3>
+          <div className="project__link">
+            <a
+              href="https://movie-city-2024.000webhostapp.com/"
+              className="project__button"
+            >
+              <i class="bx bx-link"></i> Demo{" "}
+              <i className="project__button-icon bx bx-right-arrow-alt"></i>
+            </a>
+            <a
+              href="https://github.com/kemchuoihot/city-movie"
+              className="project__button"
+            >
+              <i class="bx bxl-github"></i>Github{" "}
+              <i className="project__button-icon bx bx-right-arrow-alt"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Project;
